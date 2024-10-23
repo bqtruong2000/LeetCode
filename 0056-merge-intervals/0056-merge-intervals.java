adc5fe10d83ca4ba6ec62c2824d160cd564ede4e
Bùi Quang Trường <123917196+bqtruong2000@users.noreply.github.com>
2024-10-23 20:42:37 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> ans = new ArrayList<>();
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] >= start && intervals[i][0] <= end)
+                end = Math.max(end,intervals[i][1]);
+            else {
+                ans.add(new int[]{start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+        ans.add(new int[]{start, end});
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
