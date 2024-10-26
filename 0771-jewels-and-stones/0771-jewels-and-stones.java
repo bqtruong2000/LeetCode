1dc718f852e5ec22899fb9ba3639efe4126ac8d4
Bùi Quang Trường <123917196+bqtruong2000@users.noreply.github.com>
2024-10-26 09:31:22 +0700

    Time: 1 ms (57.4%), Space: 42.1 MB (5.22%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,18 +1,27 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        int ans = 0;
-        HashMap<Integer, String> check = new HashMap<Integer, String>();
-        for(int i = 0; i < jewels.length(); i++){
-            // System.out.println(jewels.charAt(i));
-            check.put(i,String.valueOf(jewels.charAt(i)));
-        }
-        for(String i: check.values()){
-            for(int j = 0; j < stones.length(); j++){
-                if(i.equals(String.valueOf(stones.charAt(j)))) ans++;
-            }
-        }
+        //Solution 1:
+        // int ans = 0;
+        // HashMap<Integer, String> check = new HashMap<Integer, String>();
+        // for(int i = 0; i < jewels.length(); i++){
+        //     check.put(i,String.valueOf(jewels.charAt(i)));
+        // }
+        // for(String i: check.values()){
+        //     for(int j = 0; j < stones.length(); j++){
+        //         if(i.equals(String.valueOf(stones.charAt(j)))) ans++;
+        //     }
+        // }
+        // return ans;
 
-        // System.out.println(check);
+        //Solution 2:
+        int ans = 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for(int i = 0; i < jewels.length(); i++){
+            if(!map.containsKey(jewels.charAt(i))) map.put(jewels.charAt(i), i);
+        }
+        for(int i = 0; i < stones.length(); i++){
+            if(map.containsKey(stones.charAt(i))) ans++;
+        }
         return ans;
     }
 }
