9251da6d9cd87e5075042dcd19e3506c32897be3
Bùi Quang Trường <123917196+bqtruong2000@users.noreply.github.com>
2024-10-29 20:34:07 +0700

    Time: 14 ms (30.59%), Space: 44.8 MB (20.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        /*    // Solution 1:
         char[] str1 = s.toCharArray();
         char[] str2 = t.toCharArray();
 
@@ -13,6 +14,14 @@ class Solution {
         String b = new String(str2);
 
         // return a.equals(b);
-        return Arrays.equals(str1, str2);
+        return Arrays.equals(str1, str2); */
+
+        //Solution 2:
+        if(s.length() != t.length()) return false;
+        HashMap<Character, Integer> sMap = new HashMap<>();
+        HashMap<Character, Integer> tMap = new HashMap<>();
+        for(char c: s.toCharArray()) sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+        for(char c: t.toCharArray()) tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+        return sMap.equals(tMap);
     }
 }
