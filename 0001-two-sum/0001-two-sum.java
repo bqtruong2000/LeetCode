c9910a67addaa4a717770ca8354a715b4891839e
Bùi Quang Trường <123917196+bqtruong2000@users.noreply.github.com>
2024-10-31 19:15:03 +0700

    Time: 4 ms (61.46%), Space: 44.4 MB (96.69%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);
+        }
+        for(int i = 0; i < nums.length; i++){
+            int n = target - nums[i];
+            if(map.containsKey(n) && i != map.get(n))  return new int[] {i, map.get(n)};
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
